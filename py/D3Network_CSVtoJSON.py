13be71108a5b311dd1c319403bad89953186adbd
Emily Merwin <emilymerwinajc@gmail.com>
2013-10-03 17:11:02 -0400

    Use if/else instead of try/except in python parser
    Try/except is faster if exceptions are rare - it's really fast as long
    as try works, but in this case we will find exceptions pretty often so
    if/else is better.

--- py/D3Network_CSVtoJSON.py
+++ py/D3Network_CSVtoJSON.py
@@ -14,23 +14,23 @@ num2 = 0
 for i, row in enumerate(reader):
 	#check if node is in nodeArr yet, and if not add it
 	node = row['Name']
-	try:
-		num = keyArr.index(node)
-
-	except ValueError:
+	if node not in keyArr:
 		keyArr.append(node)
 		num = len(keyArr)-1
 		nodeArr.append({"name": row['Name']})
 
+	else:
+		num = keyArr.index(node)
+
 	#check if node2 is in nodeArr yet
 	node2 = row['Name or organization']
-	try:
-		num2 = keyArr.index(node2)
-
-	except ValueError:
+	if node2 not in keyArr:
 		keyArr.append(node2)
 		num2 = len(keyArr)-1
 		nodeArr.append({"name": row['Name or organization']})
+
+	else:
+		num2 = keyArr.index(node2)
 
 	#add link between node and node2
 	linkArr.append({"source": num, "target": num2, "connection": row['Link'], "notes": row['Notes']})
